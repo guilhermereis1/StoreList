14f9b4c6a4d7730fd7abfab7c68c763ea9982d56
Guilherme <guilhermejosereis@outlook.com>
2019-04-01 14:44:18 -0400

    Add Home and Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { createMuiTheme } from "@material-ui/core/styles";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
-import AppBar from "@material-ui/core/AppBar";
 import "./App.css";
+import Header from "./common/Header";
+import Home from "./home/index";
 
 const theme = createMuiTheme({
   palette: {
@@ -19,11 +20,8 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
-        <AppBar position="static" color="primary">
-          <div className="header">
-            <p className="title">Lista de Compras</p>
-          </div>
-        </AppBar>
+        <Header />
+        <Home />
         <div className="App" />
       </MuiThemeProvider>
     );
