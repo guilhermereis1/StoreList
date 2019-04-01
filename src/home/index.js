529ead3661ae9aafe9a1f55973832a2adca1a036
Guilherme <guilhermejosereis@outlook.com>
2019-04-01 14:44:47 -0400

    Create Card

--- src/home/index.js
+++ src/home/index.js
@@ -1,10 +1,25 @@
 import React, { Component } from "react";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import "./Home.css";
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Home Page</h1>
+      <div className="page-container">
+        <div className="new-list-container">
+          <Card className="card">
+            <CardActionArea className="card-action-area">
+              <CardContent className="card-content">
+                <div>
+                  <p className="title">Adicionar Nova Lista</p>
+                </div>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
       </div>
     );
   }
